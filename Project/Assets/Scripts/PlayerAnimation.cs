fc1e796d71b2d21e4b13064edee9c326a9e1791c
KaraBeLa-LGBT <karaqqbela@gmail.com>
2021-04-20 13:00:15 +0300

    ok
    удалите из файлa script, скрипт laser

---
+++ Project/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+  
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("isRight", true);
+        }
+        else
+        {
+            anim.SetBool("isRight", false);
+        }
+    }
+}
