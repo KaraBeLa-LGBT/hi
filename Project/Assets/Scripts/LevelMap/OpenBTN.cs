b6fd00bb13d8bf389f3d727367b7b2965c2b1b68
KaraBeLa-LGBT <karaqqbela@gmail.com>
2021-06-03 16:49:15 +0300

    03.06

---
+++ Project/Assets/Scripts/LevelMap/OpenBTN.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OpenBTN : MonoBehaviour
+{
+    public int idLevel;
+    void Start()
+    {
+        int i = PlayerPrefs.GetInt("levelComplite");
+        Button btn = GetComponent<Button>();
+        if (idLevel <= i){
+            btn.interactable = true;
+        } else {
+            btn.interactable = false;
+        }
+    }
+}
---
+++ Project/Assets/Scripts/LevelMap/OpenLevel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenLevel : MonoBehaviour
+{
+    public void loadLevel(int id){
+        SceneManager.LoadScene(id);
+    }
+}
