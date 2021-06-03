13ecef56f9bf35bcf0257443bfd2ce5b3c9bb2a9
KaraBeLa-LGBT <karaqqbela@gmail.com>
2021-06-03 16:36:06 +0300

    03.06
    замена спрайтов NPC

---
+++ Project/Project/Assets/Scripts/Health.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int health = 0;
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag.Equals("Acid")){
+            Destroy(other.gameObject);
+            
+            if (health == 0){
+                Destroy(gameObject);
+            } else if (health >= 0){
+                health -= 1;
+            }
+        }
+    }
+}
