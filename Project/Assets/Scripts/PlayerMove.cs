ad0b6d28ca0e8362f96770aef2270b742827d97a
KaraBeLa-LGBT <82215728+KaraBeLa-LGBT@users.noreply.github.com>
2021-08-13 11:49:28 +0300

    Revert "Звуки"

--- Project/Assets/Scripts/PlayerMove.cs
+++ Project/Assets/Scripts/PlayerMove.cs
@@ -12,14 +12,10 @@ public class PlayerMove : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 moveVelocity;
 
-    [SerializeField] private AudioSource footsteps;
-
-    AudioSource playerAudio;
-
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        //  animator = GetComponent<Animator>();
+      //  animator = GetComponent<Animator>();
     }
 
 
@@ -27,26 +23,6 @@ public class PlayerMove : MonoBehaviour
     {
         Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         moveVelocity = moveInput.normalized * speed;
-
-        if (Input.GetKeyDown(KeyCode.A))
-            footsteps.Play();
-
-        else if (Input.GetKeyUp(KeyCode.A))
-            footsteps.Stop();
-        if (Input.GetKeyDown(KeyCode.W))
-            footsteps.Play();
-        else if (Input.GetKeyUp(KeyCode.W))
-            footsteps.Stop();
-        if (Input.GetKeyDown(KeyCode.S))
-            footsteps.Play();
-        else if (Input.GetKeyUp(KeyCode.S))
-            footsteps.Stop();
-        if (Input.GetKeyDown(KeyCode.D))
-            footsteps.Play();
-        else if (Input.GetKeyUp(KeyCode.D))
-            footsteps.Stop();
-
-
 
     }
     void FixedUpdate()
