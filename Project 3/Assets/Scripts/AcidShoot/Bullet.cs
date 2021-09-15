36204163f388e46096c95d5b25dc817003503e68
DerbYl <soslisqp@gmail.com>
2021-09-15 16:22:41 +0300

    1

---
+++ Project 3/Assets/Scripts/AcidShoot/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rigidbody2D;
+
+    public Transform transform1;
+    public Transform transform2;
+    private int len = 0;
+    void Start()
+    {
+        rigidbody2D.velocity = new Vector2(transform2.position.x - transform1.position.x,
+                                        transform2.position.y - transform1.position.y);
+    }
+}
---
+++ Project 3/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    Animator animator;
+
+    public float speed;
+
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+    [SerializeField] private AudioSource footsteps;
+
+    AudioSource playerAudio;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //  animator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+
+        if (Input.GetKeyDown(KeyCode.A))
+            footsteps.Play();
+
+        else if (Input.GetKeyUp(KeyCode.A))
+            footsteps.Stop();
+        if (Input.GetKeyDown(KeyCode.W))
+            footsteps.Play();
+        else if (Input.GetKeyUp(KeyCode.W))
+            footsteps.Stop();
+        if (Input.GetKeyDown(KeyCode.S))
+            footsteps.Play();
+        else if (Input.GetKeyUp(KeyCode.S))
+            footsteps.Stop();
+        if (Input.GetKeyDown(KeyCode.D))
+            footsteps.Play();
+        else if (Input.GetKeyUp(KeyCode.D))
+            footsteps.Stop();
+
+
+
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+}
